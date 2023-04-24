e7cd0adb2e47f97ae80d41ac304e3aafb24fb696
Dale Menciro <dale_menciro@protonmail.com>
2023-04-24 23:38:44 +0800

    deleting program directions

--- even square odd cube.py
+++ even square odd cube.py
@@ -1,10 +1,3 @@
-'''
-Write a method in python that will create two separate text files after reading 
-the source text file named integers.txt that contains 20 integers. The first output 
-file will be named double.txt containing the square of all even integers found in integers.txt
-and the second file will be named triple.txt containing the cube of all odd numbers found in the 
-integers.txt.
-'''
 def process_file():
         #read integers from integers.txt
     with open("integers.txt") as input_file:
