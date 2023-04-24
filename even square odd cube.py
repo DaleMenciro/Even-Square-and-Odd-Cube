303faaec67c985e914730955b5877d9660774fe8
Dale Menciro <dale_menciro@protonmail.com>
2023-04-24 23:13:02 +0800

    Pseudocode

--- even square odd cube.py
+++ even square odd cube.py
@@ -5,3 +5,12 @@ file will be named double.txt containing the square of all even integers found i
 and the second file will be named triple.txt containing the cube of all odd numbers found in the 
 integers.txt.
 '''
+def process_file():
+    #read integers from integers.txt
+    #extract all even and odd from the list
+    #append square of even numbers to double.txt
+    #append cube of odd numbers to triple.txt
+
+
+#====== START =====
+    process_file()
