0f413403244d8fad522f989062dfedc297b554f7
Dale Menciro <dale_menciro@protonmail.com>
2023-04-24 23:22:33 +0800

    extract even integers from the list

--- even square odd cube.py
+++ even square odd cube.py
@@ -6,12 +6,13 @@ and the second file will be named triple.txt containing the cube of all odd numb
 integers.txt.
 '''
 def process_file():
-    #read integers from integers.txt
+        #read integers from integers.txt
     with open("integers") as input_file:
         integers = list(map(int, input_file.read().split()))
-    #extract all even and odd from the list
-    #append square of even numbers to double.txt
-    #append cube of odd numbers to triple.txt
+        #extract all even and odd from the list
+        even_integers = [n for n in integers if n % 2 == 0]
+        #append square of even numbers to double.txt
+        #append cube of odd numbers to triple.txt
 
 
 #====== START =====
