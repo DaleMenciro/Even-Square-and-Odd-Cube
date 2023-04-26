e7477c14545aa42936ac72dfdfb4327fcd5893c3
Dale Menciro <dale_menciro@protonmail.com>
2023-04-26 20:19:28 +0800

    adding try and making messagebox for completion

--- even square odd cube.py
+++ even square odd cube.py
@@ -2,23 +2,27 @@ import tkinter as tk
 from tkinter import filedialog, messagebox
 
 def process_file(input_path, output_dir):
-        #read integers from integers.txt
-    with open(input_path) as input_file:
-        integers = list(map(int, input_file.read().split()))
-        #extract all even and odd from the list
-        even_integers = [n for n in integers if n % 2 == 0]
-        odd_integers = [n for n in integers if n % 2 != 0]
+    try:
+            #read integers from integers.txt
+        with open(input_path) as input_file:
+            integers = list(map(int, input_file.read().split()))
+            #extract all even and odd from the list
+            even_integers = [n for n in integers if n % 2 == 0]
+            odd_integers = [n for n in integers if n % 2 != 0]
 
-        #append square of even numbers to double.txt
-        with open(output_dir + "double.txt", "a") as double_file:
-            for n in even_integers:
-                double_file.write(str(n * n) + "\n")
-        
-        #append cube of odd numbers to triple.txt
-        with open(output_dir +"triple.txt", "a") as triple_file:
-            for n in odd_integers:
-                triple_file.write(str(n * n * n) + "\n")
-
+            #append square of even numbers to double.txt
+            with open(output_dir + "double.txt", "a") as double_file:
+                for n in even_integers:
+                    double_file.write(str(n * n) + "\n")
+            
+            #append cube of odd numbers to triple.txt
+            with open(output_dir +"triple.txt", "a") as triple_file:
+                for n in odd_integers:
+                    triple_file.write(str(n * n * n) + "\n")
+            messagebox.showinfo("Task Complete", "Even are Squared and Odds are Cubed!")
+    except Exception as e:
+        messagebox.showerror("Error", f"An error occurred: {str(e)}")     
+    
 def browse_input_file_path():
     global input_file_path
     input_file_path = filedialog.askopenfilename()
